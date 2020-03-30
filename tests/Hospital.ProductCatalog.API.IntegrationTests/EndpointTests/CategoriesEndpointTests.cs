f58b232efb0afae13630812a73d453ef456dd33f
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-03-30 20:38:48 +0200

    add A_Category_Can_Be_Updated and A_Category_Can_Be_Deleted integration test

--- tests/Hospital.ProductCatalog.API.IntegrationTests/EndpointTests/CategoriesEndpointTests.cs
+++ tests/Hospital.ProductCatalog.API.IntegrationTests/EndpointTests/CategoriesEndpointTests.cs
@@ -24,5 +24,40 @@ namespace Hospital.ProductCatalog.API.IntegrationTests.EndpointTests
             var after = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
             Assert.AreEqual(1, after.Count());
         }
+
+        [TestMethod]
+        public async Task A_Category_Can_Be_Updated()
+        {
+            var client = NewHttpClient();
+            var category = new Category { Code = 1, Description = "Consumables" };
+            var updatedCategory = new Category { Code = 1, Description = "Implants" };
+
+            await client.PostAsJsonAsync("/categories", category);
+
+            var before = await client.GetFromJsonAsync<Category>("/categories/1");
+            Assert.AreEqual(category.Description, before.Description);
+
+            await client.PutAsJsonAsync("/categories/1", updatedCategory);
+
+            var after = await client.GetFromJsonAsync<Category>("/categories/1");
+            Assert.AreEqual(updatedCategory.Description, after.Description);
+        }
+
+        [TestMethod]
+        public async Task A_Category_Can_Be_Deleted()
+        {
+            var client = NewHttpClient();
+            var category = new Category { Code = 1, Description = "Consumables" };
+
+            await client.PostAsJsonAsync("/categories", category);
+
+            var before = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
+            Assert.AreEqual(1, before.Count());
+
+            await client.DeleteAsync("/categories/1");
+
+            var after = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
+            Assert.AreEqual(0, after.Count());
+        }
     }
 }
