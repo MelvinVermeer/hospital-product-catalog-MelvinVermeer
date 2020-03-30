cf08df94c21d48b276970e877d578c9fc8367a3c
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-03-30 13:34:15 +0200

    implement categories controller (get/post/put/delete)

---
+++ src/Hospital.ProductCatalog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,86 @@
+﻿using Hospital.ProductCatalog.DataAccess;
+using Hospital.ProductCatalog.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hospital.ProductCatalog.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ProductCatalogContext _db;
+
+        public CategoriesController(ProductCatalogContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> Get()
+        {
+            return await _db.Categories.ToListAsync();
+        }
+
+        [HttpGet("{code}")]
+        public async Task<ActionResult<Category>> Get(int code)
+        {
+            var category = await _db.Categories.FirstOrDefaultAsync(x => x.Code == code);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> Post(Category category)
+        {
+            _db.Categories.Add(category);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { category.Code }, category);
+        }
+
+        [HttpPut("{code}")]
+        public async Task<ActionResult<Category>> Put(int code, Category category)
+        {
+            if (code != category.Code)
+            {
+                return BadRequest();
+            }
+
+            var dbCategory = await _db.Categories.FirstOrDefaultAsync(x => x.Code == code);
+            if (dbCategory == null)
+            {
+                return NotFound();
+            }
+
+            dbCategory.Description = category.Description;
+
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{code}")]
+        public async Task<ActionResult<Category>> Delete(int code)
+        {
+            var category = await _db.Categories.FirstOrDefaultAsync(x => x.Code == code);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- src/Hospital.ProductCatalog.API/Startup.cs
+++ src/Hospital.ProductCatalog.API/Startup.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Hospital.ProductCatalog.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +13,11 @@ namespace Hospital.ProductCatalog.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            // Let's go for an InMemory Database, this way we can use Entity Framework 
+            // all the way through and whenever needed this can easily be swapped by real MSSQL.
+            services.AddDbContext<ProductCatalogContext>(options => options.UseInMemoryDatabase("HospitalProductCatalog"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +29,10 @@ namespace Hospital.ProductCatalog.API
             }
 
             app.UseRouting();
-
+            
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
