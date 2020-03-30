0a31e2ad49867339351038d4897cc06ff5cec2eb
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-03-30 14:26:30 +0200

    add integration test A_New_Category_Can_Be_Added

---
+++ tests/Hospital.ProductCatalog.API.IntegrationTests/EndpointTests/CategoriesEndpointTests.cs
@@ -0,0 +1,28 @@
+﻿using Hospital.ProductCatalog.API.IntegrationTests.Extensions;
+using Hospital.ProductCatalog.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.ProductCatalog.API.IntegrationTests.EndpointTests
+{
+    [TestClass]
+    public class CategoriesEndPointTests : TestBase
+    {
+        [TestMethod]
+        public async Task A_New_Category_Can_Be_Added()
+        {
+            var client = NewHttpClient();
+            var category = new Category { Code = 1, Description = "Consumables" };
+
+            var before = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
+            Assert.AreEqual(0, before.Count());
+
+            await client.PostAsJsonAsync("/categories", category);
+
+            var after = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
+            Assert.AreEqual(1, after.Count());
+        }
+    }
+}
