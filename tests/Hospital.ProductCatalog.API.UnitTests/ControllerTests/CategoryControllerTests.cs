210e05455e9d64a538dc79cae6d150c55ac66943
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-03-30 16:59:18 +0200

    update unit tests to support mediatr

--- tests/Hospital.ProductCatalog.API.UnitTests/ControllerTests/CategoryControllerTests.cs
+++ tests/Hospital.ProductCatalog.API.UnitTests/ControllerTests/CategoryControllerTests.cs
@@ -1,9 +1,12 @@
 using Hospital.ProductCatalog.API.Controllers;
-using Hospital.ProductCatalog.DataAccess;
+using Hospital.ProductCatalog.BusinessLogic.Categories.Queries;
+using Hospital.ProductCatalog.BusinessLogic.Exceptions;
 using Hospital.ProductCatalog.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Linq;
+using Moq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Hospital.ProductCatalog.API.UnitTests.ControllerTests
@@ -11,29 +14,43 @@ namespace Hospital.ProductCatalog.API.UnitTests.ControllerTests
     [TestClass]
     public class CategoryControllerTests
     {
-        private Category _category = new Category { Code = 1, Description = "Consumables" };
+        private readonly Category _category = new Category { Code = 1, Description = "Consumables" };
 
         [TestMethod]
-        public async Task Get_Should_Return_All_Categories()
+        public async Task Get_Should_Send_Query_GetAll()
         {
-            var options = new DbContextOptionsBuilder<ProductCatalogContext>()
-                .UseInMemoryDatabase("Get_Should_Return_All_Categories")
-                .Options;
+            var mediatorMock = new Mock<IMediator>();
+            var controller = new CategoriesController(null, mediatorMock.Object);
 
-            using (var db = new ProductCatalogContext(options))
-            {
-                db.Categories.Add(_category);
-                db.SaveChanges();
-            }
+            await controller.Get();
 
-            using (var context = new ProductCatalogContext(options))
-            {
-                var controller = new CategoriesController(context);
-                var result = (await controller.Get()).Value;
+            mediatorMock.Verify(m => m.Send(It.IsAny<GetAll>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
 
-                Assert.AreEqual(1, result.Count());
-                Assert.AreEqual(_category.Code, result.First().Code);
-            }
+        [TestMethod]
+        public async Task Get_Should_Send_Query_GetByCode_If_Code_Provided()
+        {
+            var mediatorMock = new Mock<IMediator>();
+            var controller = new CategoriesController(null, mediatorMock.Object);
+            var code = 1;
+
+            await controller.Get(code);
+
+            mediatorMock.Verify(m => m.Send(It.Is<GetByCode>(x => x.Code == code), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task Get_Should_Return_NotFound_If_Mediator_Throws_NotFound()
+        {
+            var mediatorMock = new Mock<IMediator>();
+            mediatorMock.Setup(x => x.Send(It.IsAny<GetByCode>(), It.IsAny<CancellationToken>()))
+                .Throws(new NotFoundException("a", 1));
+            var controller = new CategoriesController(null, mediatorMock.Object);
+            var code = 1;
+
+            var result = (await controller.Get(code)).Result;
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
         }
     }
 }
