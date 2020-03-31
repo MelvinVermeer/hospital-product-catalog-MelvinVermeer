50c325052a3bf222e625a39c2ca387fe2983ca96
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-03-31 12:14:56 +0200

    use Fixture in CategoriesEndpointTests

--- tests/Hospital.ProductCatalog.API.IntegrationTests/EndpointTests/CategoriesEndpointTests.cs
+++ tests/Hospital.ProductCatalog.API.IntegrationTests/EndpointTests/CategoriesEndpointTests.cs
@@ -14,46 +14,29 @@ namespace Hospital.ProductCatalog.API.IntegrationTests.EndpointTests
         public async Task A_New_Category_Can_Be_Added()
         {
             var client = NewHttpClient();
-            var category = new Category { Code = 1, Description = "Consumables" };
-
-            var before = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
-            Assert.AreEqual(0, before.Count());
+            var category = new Category { Code = 2, Description = "Implants" };
 
             await client.PostAsJsonAsync("/categories", category);
 
-            var after = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
-            Assert.AreEqual(1, after.Count());
+            var result = await client.GetFromJsonAsync<Category>("/categories/2");
+            Assert.AreEqual(category.Description, result.Description);
         }
 
         [TestMethod]
         public async Task A_Category_Can_Be_Updated()
         {
             var client = NewHttpClient();
-            var category = new Category { Code = 1, Description = "Consumables" };
-            var updatedCategory = new Category { Code = 1, Description = "Implants" };
-
-            await client.PostAsJsonAsync("/categories", category);
-
-            var before = await client.GetFromJsonAsync<Category>("/categories/1");
-            Assert.AreEqual(category.Description, before.Description);
-
+            var updatedCategory = new Category { Code = 1, Description = "new description" };
             await client.PutAsJsonAsync("/categories/1", updatedCategory);
 
-            var after = await client.GetFromJsonAsync<Category>("/categories/1");
-            Assert.AreEqual(updatedCategory.Description, after.Description);
+            var result = await client.GetFromJsonAsync<Category>("/categories/1");
+            Assert.AreEqual(updatedCategory.Description, result.Description);
         }
 
         [TestMethod]
         public async Task A_Category_Can_Be_Deleted()
         {
             var client = NewHttpClient();
-            var category = new Category { Code = 1, Description = "Consumables" };
-
-            await client.PostAsJsonAsync("/categories", category);
-
-            var before = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
-            Assert.AreEqual(1, before.Count());
-
             await client.DeleteAsync("/categories/1");
 
             var after = await client.GetFromJsonAsync<IEnumerable<Category>>("/categories");
