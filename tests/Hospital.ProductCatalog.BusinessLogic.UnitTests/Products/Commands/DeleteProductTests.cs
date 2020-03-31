c2f057baf41bb71343232f5f49026db031ec0bef
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-03-31 10:57:44 +0200

    test UpdateProduct and DeleteProduct commands

---
+++ tests/Hospital.ProductCatalog.BusinessLogic.UnitTests/Products/Commands/DeleteProductTests.cs
@@ -0,0 +1,36 @@
+﻿using Hospital.ProductCatalog.BusinessLogic.Exceptions;
+using Hospital.ProductCatalog.BusinessLogic.Products.Commands;
+using Hospital.ProductCatalog.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.ProductCatalog.BusinessLogic.UnitTests.Products.Commands
+{
+    [TestClass]
+    public class DeleteProductTests
+    {
+        private readonly DeleteProductHandler _handler;
+        private readonly ProductCatalogContext _context;
+
+        public DeleteProductTests()
+        {
+            _context = ProductCatalogContextFactory.Create();
+            _handler = new DeleteProductHandler(_context);
+        }
+
+        [TestMethod]
+        public async Task Should_Delete_A_Product()
+        {
+            await _handler.Handle(new DeleteProduct(1));
+            Assert.AreEqual(0, _context.Products.Count());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public async Task Should_Throw_NotFound_When_Product_With_Given_Code_Does_Not_Exist()
+        {
+            await _handler.Handle(new DeleteProduct(2));
+        }
+    }
+}
---
+++ tests/Hospital.ProductCatalog.BusinessLogic.UnitTests/Products/Commands/UpdateProductTests.cs
@@ -0,0 +1,50 @@
+﻿using Hospital.ProductCatalog.BusinessLogic.Exceptions;
+using Hospital.ProductCatalog.BusinessLogic.Products.Commands;
+using Hospital.ProductCatalog.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Hospital.ProductCatalog.BusinessLogic.UnitTests.Products.Commands
+{
+    [TestClass]
+    public class UpdateProductTests
+    {
+        private readonly UpdateProductHandler _handler;
+        private readonly ProductCatalogContext _context;
+
+        public UpdateProductTests()
+        {
+            _context = ProductCatalogContextFactory.Create();
+            var mapper = MapperFactory.Create();
+            _handler = new UpdateProductHandler(_context, mapper);
+        }
+
+        [TestMethod]
+        public async Task Should_Update_A_Product_Description()
+        {
+            var command = new UpdateProduct
+            {
+                Code = Fixture.Products[0].Code,
+                Description = "Implants",
+                UnitOfMeasurement = "Box",
+                CategoryCode = Fixture.Categories[0].Code
+            };
+            await _handler.Handle(command);
+            Assert.AreEqual("Implants", _context.Products.Find(1).Description);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public async Task Should_Throw_NotFound_When_Product_With_Given_Code_Does_Not_Exist()
+        {
+            await _handler.Handle(new UpdateProduct { Code = 2 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotFoundException))]
+        public async Task Should_Throw_NotFound_If_No_Category_With_Given_Code_Exisits()
+        {
+            await _handler.Handle(new UpdateProduct { Code = 1, CategoryCode = 99 });
+        }
+    }
+}
