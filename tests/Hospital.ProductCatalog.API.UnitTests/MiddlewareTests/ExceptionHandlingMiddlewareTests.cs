ce34a345c8810cd3e3dcb81190204d4c0ced6f16
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-04-10 22:06:40 +0200

    test rest of the exception handling middleware

--- tests/Hospital.ProductCatalog.API.UnitTests/MiddlewareTests/ExceptionHandlingMiddlewareTests.cs
+++ tests/Hospital.ProductCatalog.API.UnitTests/MiddlewareTests/ExceptionHandlingMiddlewareTests.cs
@@ -1,7 +1,13 @@
-﻿using Hospital.ProductCatalog.API.Middleware;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Hospital.ProductCatalog.API.Middleware;
 using Hospital.ProductCatalog.BusinessLogic.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Hospital.ProductCatalog.API.UnitTests.MiddlewareTests
@@ -25,7 +31,7 @@ namespace Hospital.ProductCatalog.API.UnitTests.MiddlewareTests
         }
 
         [TestMethod]
-        public async Task Write_404_Response_If_NotFound_Is_Thrown_v2()
+        public async Task Write_404_Response_If_NotFound_Is_Thrown()
         {
             var exception = new NotFoundException("Category", 1);
             Task next(HttpContext context)
@@ -39,6 +45,51 @@ namespace Hospital.ProductCatalog.API.UnitTests.MiddlewareTests
 
             Assert.AreEqual("application/json", context.Response.ContentType);
             Assert.AreEqual(404, context.Response.StatusCode);
+            Assert.AreEqual(exception.Message, await context.Response.ReadAsAsync<string>());
+        }
+
+        [TestMethod]
+        public async Task Write_400_Response_If_ValidationException_Is_Thrown()
+        {
+            var failures = new List<ValidationFailure> 
+            {
+                new ValidationFailure("name", "name can not be empty")
+            };
+
+            var exception = new ValidationException(failures);
+
+            Task next(HttpContext context)
+            {
+                throw exception;
+            }
+            var middleware = new ExceptionHandlingMiddleware(next);
+            var context = HttpContextFactory.Create();
+
+            await middleware.Invoke(context);
+
+            Assert.AreEqual("application/json", context.Response.ContentType);
+            Assert.AreEqual(400, context.Response.StatusCode);
+
+            var failuresResponse = await context.Response.ReadAsAsync<List<ValidationFailure>>();
+
+            Assert.AreEqual(exception.Errors.ToList()[0].ErrorMessage, failuresResponse[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public async Task Write_500_Response_If_Anonther_Is_Thrown()
+        {
+            var exception = new Exception("input invalid");
+            Task next(HttpContext context)
+            {
+                throw exception;
+            }
+            var middleware = new ExceptionHandlingMiddleware(next);
+            var context = HttpContextFactory.Create();
+
+            await middleware.Invoke(context);
+
+            Assert.AreEqual("application/json", context.Response.ContentType);
+            Assert.AreEqual(500, context.Response.StatusCode);
             Assert.AreEqual(exception.Message, await context.Response.ReadAsAsync<string>());
         }
     }
